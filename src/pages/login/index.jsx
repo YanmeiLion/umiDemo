bcbfc0cde8c10f5ff945a6f18ac0af1f8b7c4b01
yanmei <yanmei@gmail.com>
2021-12-07 13:18:28 +0800

    pre-commit config

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Card, Tabs  } from 'antd';
-import styles from './index.less'
+import React from 'react';
+import { Card, Tabs } from 'antd';
+import styles from './index.less';
 const { TabPane } = Tabs;
 
 export default function index() {
   function callback(key) {
     console.log(key);
   }
+  // console.log('qq')
+
   return (
     <div className={styles.loginInfo}>
       <Card size="small" className={styles.card}>
-        <Tabs defaultActiveKey="1" onChange={callback} className={styles.titles}>
+        <Tabs
+          defaultActiveKey="1"
+          onChange={callback}
+          className={styles.titles}
+        >
           <TabPane tab="Tab 1" key="1">
             1111111111111111111111
           </TabPane>
@@ -23,5 +29,5 @@ export default function index() {
         </Tabs>
       </Card>
     </div>
-  )
+  );
 }
