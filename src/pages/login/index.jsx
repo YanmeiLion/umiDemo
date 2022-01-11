ed4fb7b959c20f28398616012898f27048aea5a9
yanmei <yanmei@gmail.com>
2022-01-11 11:27:08 +0800

    面包屑导航升级版本

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,44 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Tabs, Breadcrumb, Button } from 'antd';
 import styles from './index.less';
 const { TabPane } = Tabs;
+import { NavLink } from 'umi';
 
-const breadcrumbData = [
-  { id: 1, title: '自动化邮件流程编辑', url: '111' },
-  { id: 2, title: '欢迎邮件1', url: '222' },
-  { id: 3, title: '选择模板', url: '333' },
-  { id: 4, title: '编辑内容', url: '444' },
-];
+import {
+  BreadCrumbDefaultData,
+  BreadCrumbPreviousStep,
+  BreadCrumbNextStep,
+} from '@/utils/commonBreadCrumb.js';
 
 export default function index() {
   // 面包屑
-  const [newArrBreadcrumb, setNewArrBreadcrumb] = useState(
-    breadcrumbData ?? [],
-  );
+  const [newArrBreadcrumb, setNewArrBreadcrumb] = useState([]);
+
+  useEffect(() => {
+    const result = BreadCrumbDefaultData();
+    setNewArrBreadcrumb(result);
+  }, []);
 
   function callback(key) {
     console.log(key);
   }
 
   const onClickCurrentBreadcrumb = (currentIndex) => {
-    commonBreadCrumb(currentIndex);
+    // commonBreadCrumb(currentIndex);
+    const previousData = BreadCrumbPreviousStep(currentIndex);
+    setNewArrBreadcrumb(previousData);
   };
 
-  // 提取公共面包屑
-  const commonBreadCrumb = (commonIndex) => {
-    // setNewArrBreadcrumb(newArrBreadcrumb.splice(0, newArr?.length))
-    setNewArrBreadcrumb([]);
-    let newArr = [];
-    breadcrumbData?.map((item, index) => {
-      if (index <= commonIndex) {
-        newArr.push(item);
-      }
-    });
-    setNewArrBreadcrumb(newArr);
-  };
+  // // 提取公共面包屑
+  // const commonBreadCrumb = (commonIndex) => {
+  //   // setNewArrBreadcrumb(newArrBreadcrumb.splice(0, newArr?.length))
+  //   setNewArrBreadcrumb([]);
+  //   let newArr = [];
+  //   breadcrumbData?.map((item, index) => {
+  //     if (index <= commonIndex) {
+  //       newArr.push(item);
+  //     }
+  //   });
+  //   setNewArrBreadcrumb(newArr);
+  // };
 
   const nextStep = () => {
-    commonBreadCrumb(newArrBreadcrumb?.length);
+    // commonBreadCrumb(newArrBreadcrumb?.length);
+    const nextData = BreadCrumbNextStep(newArrBreadcrumb?.length);
+    setNewArrBreadcrumb(nextData);
   };
 
   return (
@@ -72,7 +79,8 @@ export default function index() {
               key={item?.id}
               onClick={() => onClickCurrentBreadcrumb(index)}
             >
-              {item?.title}
+              <NavLink to={item?.url}> {item?.title} </NavLink>
+              {/* {item?.title} */}
             </Breadcrumb.Item>
           );
         })}
---
+++ src/utils/commonBreadCrumb.js
@@ -0,0 +1,69 @@
+const breadcrumbData = [
+  { id: 1, title: '自动化邮件流程编辑', url: '/home' },
+  { id: 2, title: '欢迎邮件1', url: '222' },
+  { id: 3, title: '选择模板', url: '333' },
+  { id: 4, title: '编辑内容', url: '444' },
+];
+
+// 提取公共面包屑
+const commonBreadCrumb = (commonIndex) => {
+  // setNewArrBreadcrumb(newArrBreadcrumb.splice(0, newArr?.length))
+  // setNewArrBreadcrumb([]);
+  let newArr = [];
+  breadcrumbData?.map((item, index) => {
+    if (index <= commonIndex) {
+      newArr.push(item);
+    }
+  });
+  // setNewArrBreadcrumb(newArr);
+  return newArr;
+};
+
+// 默认面包屑导航（数据）
+export const BreadCrumbDefaultData = () => {
+  return breadcrumbData;
+};
+
+// 上一步
+export const BreadCrumbPreviousStep = (index) => {
+  return commonBreadCrumb(index);
+};
+
+// 下一步
+export const BreadCrumbNextStep = (currentLength) => {
+  return commonBreadCrumb(currentLength);
+};
+
+// 提取公共面包屑
+const commonBreadCrumb = (commonIndex, data) => {
+  // setNewArrBreadcrumb(newArrBreadcrumb.splice(0, newArr?.length))
+  let newArr = [];
+  data?.map((item, index) => {
+    if (index <= commonIndex) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+};
+
+// // 默认面包屑导航（数据）
+// export const BreadCrumbDefaultData = (name = '') => {
+//   // 面包屑数组
+//   const breadcrumbData = [
+//     { id: 1, title: '自动化邮件流程编辑', url: '/emailAutomation/editorAutomateEmails' },
+//     { id: 2, title: `${name || '欢迎邮件1'}`, url: '/emailAutomation/showAutomateEmail' },
+//     { id: 3, title: '选择模板', url: '/emailManageEditor/changeEmails' },
+//     { id: 4, title: '编辑内容', url: '/emailManageEditor/editorEmails' },
+//   ];
+//   return breadcrumbData;
+// };
+
+// // 上一步
+// export const BreadCrumbPreviousStep = (index, data) => {
+//   return commonBreadCrumb(index, data);
+// };
+
+// // 下一步
+// export const BreadCrumbNextStep = (currentLength, data) => {
+//   return commonBreadCrumb(currentLength, data);
+// };
